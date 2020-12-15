323ace26d463e818f805b3a0b12fa9018a40381f
Rivaltino Arron <arron2501@gmail.com>
2020-12-15 18:47:26 +0700

    Add products page, and routing the navbar

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '100% QUALITY CUSTOM CASES | ')
+@section('about_status', 'active')
+@section('fixed-top', 'fixed-top')
+@section('footer_status', 'fixed-bottom')
+@section('css')
+    <link href="{{ asset('css/about.css') }}" rel="stylesheet">
+@stop
+
+@section('content')
---
+++ resources/views/faq.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '100% QUALITY CUSTOM CASES | ')
+@section('faq_status', 'active')
+@section('fixed-top', 'fixed-top')
+@section('footer_status', 'fixed-bottom')
+@section('css')
+    <link href="{{ asset('css/faq.css') }}" rel="stylesheet">
+@stop
+
+@section('content')
